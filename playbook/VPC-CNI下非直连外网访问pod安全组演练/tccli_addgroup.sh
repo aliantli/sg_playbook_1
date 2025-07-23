d7363a481c30b3c467e6ffef3823509325fa29e2
aliantli <3390894629@qq.com>
2025-07-23 16:59:58 +0800

    Update tccli_addgroup.sh

--- playbook/VPC-CNI下非直连外网访问pod安全组演练/tccli_addgroup.sh
+++ playbook/VPC-CNI下非直连外网访问pod安全组演练/tccli_addgroup.sh
@@ -5,18 +5,63 @@ group1=`tccli vpc CreateSecurityGroup --cli-unfold-argument   --GroupName TestGr
 echo "绑定到节点安全组"
 echo $group1
 echo $group1 >>group.txt
-a=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group1   --SecurityGroupPolicySet '{"Ingress": [{"Protocol": "ALL","CidrBlock": "0.0.0.0/0","Action": "DROP","PolicyIndex": 0,"Action": "DROP","PolicyIndex": 0}]}'`
-b=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group1   --SecurityGroupPolicySet '{"Egress": [{"PolicyIndex": 0,"Protocol": "ALL","CidrBlock": "0.0.0.0/0","Action": "ACCEPT"}]}'`
+rule1="'{
+"Ingress": [{
+"Protocol": "ALL",
+"CidrBlock": "0.0.0.0/0",
+"Action": "DROP",
+"PolicyIndex": 0,
+"Action": "DROP",
+"PolicyIndex": 0
+}]}'"
+rule2="'{
+"Egress": [{
+"PolicyIndex": 0,
+"Protocol": "ALL",
+"CidrBlock": "0.0.0.0/0",
+"Action": "ACCEPT"
+}]}'"
+a=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group1   --SecurityGroupPolicySet $rule1`
+b=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group1   --SecurityGroupPolicySet $rule2`
 group2=`tccli vpc CreateSecurityGroup --cli-unfold-argument   --GroupName TestGroup      --GroupDescription test-group-desc  --Tags.0.Value $value   --Tags.0.Key  $key  | jq -r '.SecurityGroup.SecurityGroupId' `
 echo "绑定到eni安全组"
 echo  $group2
 echo $group2 >>group.txt
-c=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group2   --SecurityGroupPolicySet '{"Ingress": [{"Protocol": "TCP","Port":80,"CidrBlock": "0.0.0.0/0","Action": "ACCEPT","PolicyIndex": 0}]}'`
-d=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group2   --SecurityGroupPolicySet '{"Egress": [{"PolicyIndex": 0,"Protocol": "ALL","CidrBlock": "0.0.0.0/0","Action": "ACCEPT"}]}'`
+rule3="'{
+"Ingress": [{
+"Protocol": "TCP",
+"Port":80,
+"CidrBlock": "0.0.0.0/0",
+"Action": "ACCEPT",
+"PolicyIndex": 0
+}]}'"
+rule4="'{
+"Egress": [{
+"PolicyIndex": 0,
+"Protocol": "ALL",
+"CidrBlock": "0.0.0.0/0",
+"Action": "ACCEPT"
+}]}'"
+c=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group2   --SecurityGroupPolicySet $rule3`
+d=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group2   --SecurityGroupPolicySet $rule4`
 group3=`tccli vpc CreateSecurityGroup --cli-unfold-argument   --GroupName TestGroup      --GroupDescription test-group-desc  --Tags.0.Value $value   --Tags.0.Key  $key  | jq -r '.SecurityGroup.SecurityGroupId' `
 echo "绑定到clb安全组"
 echo $group3
 echo $group3 >>group.txt
-e=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group3   --SecurityGroupPolicySet '{"Ingress": [{"Protocol": "TCP","Port":22,"CidrBlock": "0.0.0.0/0","Action": "ACCEPT","PolicyIndex": 0}]}'`
-f=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group3   --SecurityGroupPolicySet '{"Egress": [{"PolicyIndex": 0,"Protocol": "ALL","CidrBlock": "0.0.0.0/0","Action": "ACCEPT"}]}'`
+
+rule5="'{
+"Ingress": [{
+"Protocol": "TCP",
+"Port":22,
+"CidrBlock": "0.0.0.0/0",
+"Action": "ACCEPT",
+"PolicyIndex": 0}]}'"
+rule6="'{
+"Egress":[{
+"PolicyIndex": 0,
+"Protocol": "ALL",
+"CidrBlock": "0.0.0.0/0",
+"Action": "ACCEPT"}]}'"
+e=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group3   --SecurityGroupPolicySet $rule5`
+f=`tccli vpc CreateSecurityGroupPolicies  --SecurityGroupId $group3   --SecurityGroupPolicySet $rule6`
 echo  "安全组创建完成"
