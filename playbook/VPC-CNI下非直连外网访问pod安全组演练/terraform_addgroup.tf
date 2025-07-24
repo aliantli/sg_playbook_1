eeb22b75c26e67aa8ddbea704d2ab37c43615712
aliantli <3390894629@qq.com>
2025-07-24 17:15:50 +0800

    Update terraform_addgroup.tf

--- playbook/VPC-CNI下非直连外网访问pod安全组演练/terraform_addgroup.tf
+++ playbook/VPC-CNI下非直连外网访问pod安全组演练/terraform_addgroup.tf
@@ -1,33 +1,4 @@
-##
-# 安全组1：仅允许 TCP 80 入站，出站全放通
-resource "tencentcloud_security_group" "web_sg" {
-  name        = "allow-http-only"
-  description = "仅允许 TCP 80 入站，出站全放通"
-  tags = {
-    key = "values"     ##配置标签
-  }
-}
-
-resource "tencentcloud_security_group_rule" "web_ingress" {
-  security_group_id = tencentcloud_security_group.web_sg.id
-  type              = "ingress"
-  cidr_ip           = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  port_range        = "80"    # 开放 HTTP 端口
-  policy            = "ACCEPT"
-  description       = "允许 HTTP 入站"
-}
-
-resource "tencentcloud_security_group_rule" "web_egress" {
-  security_group_id = tencentcloud_security_group.web_sg.id
-  type              = "egress"
-  cidr_ip           = "0.0.0.0/0"
-  ip_protocol       = "ALL"   # 允许所有协议出站
-  policy            = "ACCEPT"
-  description       = "允许所有出站流量"
-}
-
-# 安全组2：仅允许 TCP 22 入站，出站全放通
+# 安全组1：仅允许 TCP 22 入站，出站全放通
 resource "tencentcloud_security_group" "mgmt_sg" {
   name        = "allow-ssh-only"
   description = "仅允许 SSH 22 入站，出站全放通"
@@ -55,7 +26,7 @@ resource "tencentcloud_security_group_rule" "ssh_egress" {
   description       = "允许所有出站流量"
 }
 
-# 安全组3：拒绝所有入站，出站全放通
+# 安全组2：拒绝所有入站，出站全放通
 resource "tencentcloud_security_group" "baseline_sg" {
   name        = "deny-all-inbound"
   description = "拒绝所有入站，出站全放通"
@@ -85,14 +56,8 @@ resource "tencentcloud_security_group_rule" "allow_egress" {
 
 # 输出安全组 ID
 # 输出安全组 ID
-output "将此安全组绑定到eni上" {
-  value = tencentcloud_security_group.web_sg.id
-}
 
 output "将此安全组绑定到clb上" {
   value = tencentcloud_security_group.mgmt_sg.id
 }
 
-output "将此安全组绑定到节点上" {
-  value = tencentcloud_security_group.baseline_sg.id 
-}
