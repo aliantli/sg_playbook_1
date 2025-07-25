93ab209743f7d11c30d3067412539647976a4162
aliantli <3390894629@qq.com>
2025-07-25 17:16:46 +0800

    Create deploy_service.sh

---
+++ playbook/VPC-CNI下非直连外网访问pod安全组演练/deploy_service.sh
@@ -0,0 +1,6 @@
+CLB_ID=`terraform state show tencentcloud_security_group.mgmt_sg | grep id|head -1|awk -F'"' '{printf $2}'`  #获取安全组id
+NODE_NAME=`kubectl get nodes -l test11=test21 -o jsonpath='{.items[*].metadata.name}'`    #获取创建节点名字
+sed -i "s/node_name/$NODE_NAME/g" deployment.yaml  
+sed -i "s/sg-id/$CLB_ID/g" addservice.yaml 
+kubectl apply -f deployment.yaml
+kubectl apply -f addservice.yaml
