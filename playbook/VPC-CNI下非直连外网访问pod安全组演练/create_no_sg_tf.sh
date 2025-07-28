d64ed0b75e1d2b5541ee1ce8fc87d95f171863da
aliantli <3390894629@qq.com>
2025-07-28 11:58:57 +0800

    Update create_no_sg_tf.sh

--- playbook/VPC-CNI下非直连外网访问pod安全组演练/create_no_sg_tf.sh
+++ playbook/VPC-CNI下非直连外网访问pod安全组演练/create_no_sg_tf.sh
@@ -2,5 +2,5 @@ read -p'[演练]输入集群标签的key值:' key
 read -p'[演练]输入集群标签的values值:' values
 read -p'[演练]输入集群id:' cls_id
 read -p'[演练]输入集群的子网id:' sub_net
-sed -e "s/<key>/$key/g" -e "s/<values>/$values/g" -e "s/<cls-id>/$cls_id/g" -e "s/<sub-id>/$sub_net/g" node_sg.template > no_sg.tf        
-terraform apply -auto-approve
+sed -e "s/<key>/$key/g" -e "s/<values>/$values/g" -e "s/<cls-id>/$cls_id/g" -e "s/<sub-id>/$sub_net/g" node_sg.template > no_sg.tf        ##根据read输入内容生成terraform的tf文件 
+
