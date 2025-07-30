61060371cc914ca65cb225460509c52843a0ed7d
aliantli <3390894629@qq.com>
2025-07-30 15:25:51 +0800

    Create setup_podyaml.sh

---
+++ playbook/GlobalRouter_PodAccessPod/setup_podyaml.sh
@@ -0,0 +1,4 @@
+IP1=`kubectl get nodes -l test11=test21 -o jsonpath='{.items[*].metadat<get nodes -l test11=test21 -o jsonpath='{.items[*].metadata.name}'|awk '{print $1}'`
+IP2=`kubectl get nodes -l test11=test21 -o jsonpath='{.items[*].metadat<get nodes -l test11=test21 -o jsonpath='{.items[*].metadata.name}'|awk '{print $2}'`
+sed -i 's/<node_ip1>/$IP1/g' pod.yaml
+sed -i 's/<node_ip2>/$IP2/g' pod.yaml
