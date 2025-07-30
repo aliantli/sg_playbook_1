47ed8eeeca86e4d4d4a423b739fa4beae86877a5
aliantli <3390894629@qq.com>
2025-07-30 15:43:54 +0800

    Update and rename create_node_tf.sh to create_node_sg_tf.sh

--- playbook/GlobalRouter_PodAccessPod/create_node_tf.sh
+++
@@ -1,3 +0,0 @@
-read -p'[演练]输入集群id:' cls_id
-read -p'[演练]输入集群的子网id:' sub_net
-sed  -e "s/<cls-id>/$cls_id/g" -e "s/<sub-id>/$sub_net/g" create_node.template > create_node.tf        ##根据read输入内容生成terraform的tf文件 
