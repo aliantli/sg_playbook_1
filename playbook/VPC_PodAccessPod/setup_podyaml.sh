beeb1bf33b5dcf49f9925801bf721ba6b340e20c
aliantli <3390894629@qq.com>
2025-07-30 14:22:26 +0800

    Update setup_podyaml.sh

--- playbook/VPC_PodAccessPod/setup_podyaml.sh
+++ playbook/VPC_PodAccessPod/setup_podyaml.sh
@@ -1,4 +1,4 @@
 IP1=`kubectl get pods -o wide|awk '{printf $1"\t"$6"\n"}'|grep -v "NAME"|grep -v IP|head -1`
 IP2=`kubectl get pods -o wide|awk '{printf $1"\t"$6"\n"}'|grep -v "NAME"|grep -v IP|tail -1`
-sed -i 's/<pod-ip1>/$IP1/g' pod.yaml
-sed -i 's/<pod-ip2>/$IP2/g' pod.yaml
+sed -i 's/<pod_ip1>/$IP1/g' pod.yaml
+sed -i 's/<pod_ip2>/$IP2/g' pod.yaml
