100e392c24ed6b9b5ab759e7eb95782bf59f97b9
aliantli <3390894629@qq.com>
2025-07-31 20:31:16 +0800

    Update setup_deploy_yaml.sh

--- playbook/VPC_Super_PodAccessPod/setup_deploy_yaml.sh
+++ playbook/VPC_Super_PodAccessPod/setup_deploy_yaml.sh
@@ -1,4 +1,4 @@
 IP1=`kubectl get nodes -l label1=value1 -o jsonpath='{.items[*].metadata.name}'|awk '{print $1}'`
-IP2=`kubectl get nodes -l label1=value1 -o jsonpath='{.items[*].metadata.name}'|awk '{print $2}'`
+IP2=`kubectl get nodes -l label2=value2 -o jsonpath='{.items[*].metadata.name}'|awk '{print $1}'`
 sed -i 's/<node_ip1>/$IP1/g' pod.yaml
 sed -i 's/<node_ip2>/$IP2/g' pod.yaml
