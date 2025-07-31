1bd9f28050077364fdedf0d482be86ee7f4bc2f7
aliantli <3390894629@qq.com>
2025-07-31 18:37:54 +0800

    Update setup_deploy_yaml.sh

--- playbook/GlobalRouter_NodeAccessPod/setup_deploy_yaml.sh
+++ playbook/GlobalRouter_NodeAccessPod/setup_deploy_yaml.sh
@@ -1,2 +1,2 @@
-IP1=`kubectl get nodes -l test11=test21 -o jsonpath='{.items[*].metadata.name}'`
+IP1=`kubectl get nodes -l test11=test21 -o jsonpath='{.items[*].metadata.name}'`|awk '{ print $2}'
 sed -i 's/<node_ip1>/$IP1/g' deployment.yaml
